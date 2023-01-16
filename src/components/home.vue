5c83306048701f10f5a0eeeebc9b75fffa423a61
Kesang97 <kesangs99@gmail.com>
2023-01-16 17:31:58 +0530

    changes

---
+++ src/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
