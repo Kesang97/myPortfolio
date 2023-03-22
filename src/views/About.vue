1583f2091e3c5c68b98d189bc45e44c79ff6f50b
Kesang97 <kesangs99@gmail.com>
2023-03-22 16:46:51 +0530

    AboutUs page added

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,21 @@
 <template>
   <section>
-    <v-container>
+    <v-container class="mt-8">
       <v-row>
-        <v-col>
-          
+        <v-col cols="12" md="5">
+          <v-img contain src="../assets/aboutme-Bg.jpg" width="300px" class="mx-auto">
+          </v-img>
+        </v-col>
+        <v-col cols="12" md="7">
+          <div class="text-h4 text-center font-weight-bold">
+            <div class="text-subtitle">About Me</div>
+              <div class="mx-auto my-1 black rounded" style="width:60px; height:7px;">
+            </div>
+          <div class="text-h6 text--secondary py-3 text-justify">
+            Hello i m Kesang Sherpa , currently working for On-track Monthly rentals company
+            located at Bangalore, indiranagar 9th main, HAL-3 , 560008
+          </div>
+          </div>
         </v-col>
       </v-row>
     </v-container>
