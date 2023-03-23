5545066e2121cfca5371f0620bbd730032f20850
Kesang97 <kesangs99@gmail.com>
2023-03-23 17:44:13 +0530

    Navbar changes + profile added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,41 +1,57 @@
 <template>
- <nav>
+  <nav>
     <v-app-bar app dark elevate-on-scroll>
-      <!-- <v-app-bar-nav-icon 
-        class="grey--text"
-        @click="drawer= !drawer">
-      </v-app-bar-nav-icon> -->
-    <v-container class="d-flex ma-0 pa-0 mx-md-auto px-md-auto">
-      <div class="my-auto hidden-md-and-up">
-        <v-icon size="30" class="mr-2" @click.stop="drawer= !drawer">mdi-menu</v-icon>
-      </div>
-    
-    <div class="my-auto">
-      <div class="text-h6 grey--text font-weight-bold">Portfolio</div>
-    </div>  
-    <v-spacer></v-spacer>
+      <v-container class="d-flex ma-0 pa-0 mx-md-auto px-md-auto">
+        <div class="my-auto hidden-md-and-up">
+          <v-icon size="30" class="mr-2" @click.stop="drawer = !drawer"
+            >mdi-menu</v-icon
+          >
+        </div>
 
-    <v-btn to="/" class="my-auto hidden-md-and-down ml-4" text>
-      Home
-    </v-btn>
-    <v-btn to="/about" class="my-auto hidden-md-and-down ml-4" text>
-      About
-    </v-btn>
-    
+        <div class="my-auto">
+          <div class="text-h6 grey--text font-weight-bold">Portfolio</div>
+        </div>
+        <v-spacer></v-spacer>
 
-    </v-container>
-
+        <v-btn to="/" class="my-auto hidden-md-and-down ml-4" text>
+          Home
+        </v-btn>
+        <v-btn to="/about" class="my-auto hidden-md-and-down ml-4" text>
+          About
+        </v-btn>
+      </v-container>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" absolute temporary class="grey lighten-2">
-      <v-list>
-        <v-list-item-group v-model="selectItem">
-          <v-list-item v-for="(item, i) in assets" :key="i" @click="$router.push(item.route)">
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      class="grey lighten-2"
+    >
+      <v-list nav dense>
+        <v-list-item class="my-2">
+          <v-list-item-avatar size="50">
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg">
+            </v-img>
+          </v-list-item-avatar>
+          <v-list-item-title>
+            <div class="text-subtitle">Kelzang Sherpa</div>
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+      <v-list nav dense>
+        <v-list-item-group color="black" v-model="selectItem">
+          <v-list-item
+            v-for="(item, i) in assets"
+            :key="i"
+            class="mb-3"
+            @click="$router.push(item.route)"
+          >
             <v-list-item-actions>
-              <v-icon left size="25" color="deep-orange">{{ item.icon }}</v-icon>
+              <v-icon left size="25" color="black">{{ item.icon }}</v-icon>
             </v-list-item-actions>
             <v-list-item-content>
-              <v-list-item-title class="orange--text">{{ item.title }}</v-list-item-title>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
@@ -45,24 +61,25 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      selectItem: 0,
 
-  export default {
-    data () {
-      return{
-        drawer: false,
-        selectItem: 0,
-
-        assets: [
-          {
-            icon:'mdi-home-account', title: 'Home', route: '/'
-          },
-          {
-            icon:'mdi-application-edit-outline', title: 'About', route: '/about'
-          },
-        ]
-      }
-    }
-     
-  
+      assets: [
+        {
+          icon: "mdi-home-account",
+          title: "Home",
+          route: "/",
+        },
+        {
+          icon: "mdi-application-edit-outline",
+          title: "About",
+          route: "/about",
+        },
+      ],
+    };
+  },
 };
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,29 @@
 <template>
   <section>
-    <v-container class="mt-8">
+    <v-container class="mt-8 mb-10">
       <v-row>
         <v-col cols="12" md="5">
-          <v-img contain src="../assets/aboutme-Bg.jpg" width="300px" class="mx-auto">
+          <v-img contain src="../assets/aboutme-Bg.jpg" width="300px" class="about-Img mb-3 mb-md-0 mx-auto">
           </v-img>
         </v-col>
         <v-col cols="12" md="7">
-          <div class="text-h4 text-center font-weight-bold">
-            <div class="text-subtitle">About Me</div>
-              <div class="mx-auto my-1 black rounded" style="width:60px; height:7px;">
-            </div>
-          <div class="text-h6 text--secondary py-3 text-justify">
-            Hello i m Kesang Sherpa , currently working for On-track Monthly rentals company
-            located at Bangalore, indiranagar 9th main, HAL-3 , 560008
-          </div>
-          </div>
+          <v-card outlined class="rounded-0" style="border: 2px dashed grey">
+            <v-container>
+              <div class="text-h4 text-center font-weight-bold">
+                <div class="text-subtitle">About Me</div>
+                  <div class="mx-auto my-1 black rounded" style="width:60px; height:7px;">
+                </div>
+              </div>
+              <div class="subtitle-1 text-center text--secondary font-weight-bold pt-3 pb-3">
+                Hello i m Kelzang Sherpa , currently working for On-track Monthly rentals company
+                located at Bangalore, indiranagar 9th main, HAL-3 , 560008.
+                Since i have started my software developer carrier from august 2022 and its been completed 6 months till now. Here i m responsible for creating a UI page , mainting or modifiying a layout for ongoing Projects and using Git as a version control to generate a PR for code integration.
+              </div>
+              
+            </v-container>
+            
+          </v-card>
+          
         </v-col>
       </v-row>
     </v-container>
@@ -31,3 +39,15 @@ export default {
   }
 }
 </script>
+
+<style>
+
+@media only screen and (max-width: 480px){
+  .about-Img{
+    width: 100%;
+    height: 280px;
+  }
+}
+
+
+</style>
